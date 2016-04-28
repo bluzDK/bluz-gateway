879c124674e832d4d37d1f5c2cb3cbbd88e943fe
Daniel Sullivan <mumblepins@gmail.com>
2016-04-28 15:13:39 +0000

    Various code cleanups

--- index.js
+++ index.js
@@ -1,155 +1,146 @@
 #!/usr/bin/env node
-var noble = require('noble');
-var express = require('express');
-var log = require('loglevel');
-var settings=require('./settings.js');
-var BluzDKModule = require('./bluz-dk-module.js');
 
-var debuglevel= settings.get('debug');
-var loggingLevels= ['trace','debug','info','warn','error'];
-if (loggingLevels.indexOf(debuglevel) > -1) {
-    log.setDefaultLevel(debuglevel);
+var noble = require( 'noble' );
+var express = require( 'express' );
+var log = require( 'loglevel' );
+var settings = require( './settings.js' );
+var BluzDKModule = require( './bluz-dk-module.js' );
+
+var debuglevel = settings.get( 'DEBUG' );
+var loggingLevels = [ 'trace', 'debug', 'info', 'warn', 'error' ];
+if ( loggingLevels.indexOf( debuglevel ) > -1 ) {
+  log.setDefaultLevel( debuglevel );
 } else {
-    log.setDefaultLevel('error');
+  log.setDefaultLevel( 'error' );
 }
 
-var server = express();
-
+var server = express( );
 
 const BLUZ_SERVICE_UUID = '871e022338ff77b1ed419fb3aa142db2';
 
-var serverPort = settings.get('serverPort');
+var serverPort = settings.get( 'serverPort' );
 
-// const BLUZ_SERVICE_UUID = '871e022338ff77b1ed419fb3aa142db3'; // WRONG: USED FOR TESTING ONLY
+noble.on( 'stateChange', function ( state ) {
+  if ( state === 'poweredOn' ) {
+    startScanning( );
+  } else {
+    noble.stopScanning( );
+  }
 
-//~ var BLUZ_WRITE_CHAR = '871e022538ff77b1ed419fb3aa142db2';
-//~ var BLUZ_READ_CHAR = '871e022438ff77b1ed419fb3aa142db2';
+} );
 
-
-
-noble.on('stateChange', function(state) {
-    if (state === 'poweredOn') {
-        startScanning();
-    } else {
-        noble.stopScanning();
-    }
-
-});
-
-function startScanning() {
-    noble.startScanning([], true);
+function startScanning( ) {
+  noble.startScanning( [ ], true );
 };
 
-noble.on('warning', function(message) {
-    log.info('Master noble warning:', message)
-});
-
+noble.on( 'warning', function ( message ) {
+  log.warn( 'Master: noble warning:', message )
+} );
 
 var peripheralList = {};
-var shuttingDown=false;
+var shuttingDown = false;
 
-
-server.get('/connected-devices', function(req, res) {
-    var jsonreturn={};
-    log.debug('Server Got request with headers',req.headers);
-//    log.debug(peripheralList);
-    for (var index in peripheralList) {
-        periph=peripheralList[index].dkModule;
-        if (periph != null) {
-            jsonreturn[periph.id] = {
-                "id": periph.id,
-                "particle-id": periph.particleId,
-                "rssi": periph.rssi,
-                "uptime": (Date.now() - periph.connectedTime)/1000
-            };
-        }
+server.get( '/connected-devices', function ( req, res ) {
+  var jsonreturn = {};
+  log.debug( 'Server Got request with headers', req.headers );
+  //    log.debug(peripheralList);
+  for ( var index in peripheralList ) {
+    periph = peripheralList[ index ].dkModule;
+    if ( periph != null ) {
+      jsonreturn[ periph.id ] = {
+        "id": periph.id,
+        "particle-id": periph.particleId,
+        "rssi": periph.rssi,
+        "uptime": ( Date.now( ) - periph.connectedTime ) / 1000
+      };
     }
-    res.contentType('application/json');
-    res.set("Access-Control-Allow-Origin", "*");  // allow access from other ports
-    res.send(JSON.stringify(jsonreturn));
-});
+  }
+  res.contentType( 'application/json' );
+  res.set( "Access-Control-Allow-Origin", "*" ); // allow access from other ports
+  res.send( JSON.stringify( jsonreturn ) );
+} );
 
-var serverApp= server.listen(serverPort, function () {
-    log.debug("Started server on port", serverPort);
-});
+var serverApp = server.listen( serverPort, function ( ) {
+  log.debug( "Started server on port", serverPort );
+} );
 
+function deletePeripheral( id ) {
 
-function deletePeripheral(id) {
-    
-    // TODO: Replace with event based deletion
-//    if (!removing[id]) {
-//        removing[id] = true;
-//    } else {
-//        return;
-//    }
-    noble.stopScanning(); // Not sure if necessary, but probably doesn't hurt
-    if (peripheralList[id]) {
+  // TODO: Replace with event based deletion
+  //    if (!removing[id]) {
+  //        removing[id] = true;
+  //    } else {
+  //        return;
+  //    }
+  noble.stopScanning( ); // Not sure if necessary, but probably doesn't hurt
+  if ( peripheralList[ id ] ) {
 
-        log.info('removing peripheral', id);
-        delete peripheralList[id].dkModule;
-        //setTimeout(function() {
-            delete peripheralList[id];
-        //    delete removing[id];
-        //}, 1500);
-    }
-    if (!shuttingDown)
-        noble.startScanning();
+    log.info( 'Master: Removing peripheral:', id );
+    delete peripheralList[ id ].dkModule;
+    //setTimeout(function() {
+    delete peripheralList[ id ];
+    //    delete removing[id];
+    //}, 1500);
+  }
+  if ( !shuttingDown )
+    noble.startScanning( );
 }
 
-noble.on('discover', function(peripheral) {
-    if (!peripheralList[peripheral.id] && settings.get('blacklist').indexOf(peripheral.id)<0) {
+noble.on( 'discover', function ( peripheral ) {
+  if ( !peripheralList[ peripheral.id ] && settings.get( 'blacklist' ).indexOf( peripheral.id ) < 0 ) {
 
-        noble.stopScanning(); // turn off scanning while connecting HACK?
+    noble.stopScanning( ); // turn off scanning while connecting HACK?
 
-        log.warn('Master found peripheral with ID ' + peripheral.id + ' and Name ' + peripheral.advertisement.localName);
+    log.info( 'Master: Found peripheral with ID ' + peripheral.id + ' and Name ' + peripheral.advertisement.localName );
 
-        peripheralList[peripheral.id] = {
-            found: true,
-            dkModule: null
-        };
+    peripheralList[ peripheral.id ] = {
+      found: true,
+      dkModule: null
+    };
 
-        peripheral.connect(function(error) {
-            peripheral.discoverServices([BLUZ_SERVICE_UUID], function(error, services) {
+    peripheral.connect( function ( error ) {
+      peripheral.discoverServices( [ BLUZ_SERVICE_UUID ], function ( error, services ) {
 
-                log.trace(services);
-                startScanning(); // connected, resume scanning
-                if (services.length > 0) {
-                    log.info('Peripheral a bluz');
-                    var bluzMod = new BluzDKModule(peripheral, function() {
-                        deletePeripheral(peripheral.id);
-                    });
-                    //~ 
-                    peripheralList[peripheral.id].dkModule = bluzMod;
-                } else {
-                    log.info('Peripheral not a Bluz');
-                    peripheral.disconnect();
-                }
-            });
+        log.trace( services );
 
-        });
-    }
-});
+        if ( services.length > 0 ) {
+          log.info( 'Peripheral a bluz' );
+          var bluzMod = new BluzDKModule( peripheral, function ( ) {
+            deletePeripheral( peripheral.id );
+          } );
 
+          peripheralList[ peripheral.id ].dkModule = bluzMod;
+        } else {
+          log.info( 'Peripheral not a Bluz' );
+          peripheral.disconnect( );
+        }
 
-if (process.platform === "win32") {
-  var rl = require("readline").createInterface({
+        startScanning( ); // connected, resume scanning
+      } );
+
+    } );
+  }
+} );
+
+if ( process.platform === "win32" ) {
+  var rl = require( "readline" ).createInterface( {
     input: process.stdin,
     output: process.stdout
-  });
+  } );
 
-  rl.on("SIGINT", function () {
-    process.emit("SIGINT");
-  });
+  rl.on( "SIGINT", function ( ) {
+    process.emit( "SIGINT" );
+  } );
 }
 
-process.on("SIGINT", function () {
-    noble.stopScanning();
-    shuttingDown=true;
-    for (var key in peripheralList) {
-        peripheralList[key].dkModule.peripheral.disconnect();
-        peripheralList[key].dkModule.client.end();
-    }
-    serverApp.close();
+process.on( "SIGINT", function ( ) {
+  noble.stopScanning( );
+  shuttingDown = true;
+  for ( var key in peripheralList ) {
+    peripheralList[ key ].dkModule.peripheral.disconnect( );
+    peripheralList[ key ].dkModule.client.end( );
+  }
+  serverApp.close( );
   //graceful shutdown
-    setTimeout(process.exit,1000);
-});
+  setTimeout( process.exit, 1500 );
+} );
--- settings.js
+++ settings.js
@@ -1,56 +1,51 @@
 'use strict';
-var fs = require('fs');
-var settings = require('nconf');
-var os = require('os');
-var path = require('path');
+var fs = require( 'fs' );
+var settings = require( 'nconf' );
+var os = require( 'os' );
+var path = require( 'path' );
+var log = require( 'loglevel' );
 
-var mkdirSync = function (path) {
+var mkdirSync = function ( path ) {
   try {
-    fs.mkdirSync(path);
-  } catch(e) {
+    fs.mkdirSync( path );
+  } catch ( e ) {
     if ( e.code != 'EEXIST' ) throw e;
   }
 }
 
-var homedir = os.homedir();
-//~ console.log(homedir);
-var settingsDir=path.join(homedir, '.bluz-gw');
-
-mkdirSync(settingsDir);
-
-var settingsFile = path.join(settingsDir,'config.json');
+var homedir = os.homedir( );
+var settingsDir = path.join( homedir, '.bluz-gw' );
+mkdirSync( settingsDir );
+var settingsFile = path.join( settingsDir, 'config.json' );
 
 var defaultSettings = {
-    'serverPort': 3000,
-    'cloud:host': 'device.spark.io',
-    'cloud:port': 5683,
-    'blacklist': [],  // note: based on BL MAC Address
-    'serverEnabled': true,
-    'debug': 'debug'
+  'serverPort': 3000,
+  'cloud:host': 'device.spark.io',
+  'cloud:port': 5683,
+  'blacklist': [ ], // note: based on BL MAC Address
+  'serverEnabled': true,
+  'DEBUG': 'warn'
 }
 
-settings.argv();
-settings.env();
-settings.file(settingsFile);
+settings.argv( );
+settings.env( );
+settings.file( settingsFile );
 
-for (var key in defaultSettings) {
-    if(!settings.get(key))
-        settings.set(key, defaultSettings[key])
+for ( var key in defaultSettings ) {
+  if ( !settings.get( key ) )
+    settings.set( key, defaultSettings[ key ] )
 }
-//~ console.log(settings.get());
 
-
-settings.saveSettings=function() {
-    //~ console.log(this);
-    this.save(function (err) {
-         if (err) {
-      console.error(err.message);
+settings.saveSettings = function ( ) {
+  this.save( function ( err ) {
+    if ( err ) {
+      log.error( err.message );
       return;
     }
-    console.log('Configuration saved successfully.');
-    });
+    log.info( 'Configuration saved successfully.' );
+  } );
 }
 
-settings.saveSettings();
+settings.saveSettings( );
 
 module.exports = settings;
